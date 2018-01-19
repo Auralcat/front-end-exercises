4b11691b8945715a65b195dcbdac0acacee6a0b5
Auralcat <ashleyzt@hi2.in>
2018-01-19 11:30:03 -0200

    Start jQuery conversion

--- mouse-coordinates/scripts/main.js
+++ mouse-coordinates/scripts/main.js
@@ -7,14 +7,18 @@
 const text = document.querySelector('.mainText');
 const mainContainer = document.querySelector('.container');
 
-mainContainer.addEventListener('mousemove', runEvent);
-
 // Event handler
-function runEvent(e) {
+mainContainer.addEventListener('mouseover', function(e) {
     e.preventDefault();
     // Print coordinates of the mouse on move on the target element
     text.textContent = `MouseX: ${e.offsetX}, MouseY: ${e.offsetY}`;
 
     // Change body background color taking the coordinates as values of rgb;
     document.body.style.backgroundColor = `rgb(${e.offsetX}, 180, ${e.offsetY})`;
-}
+});
+
+// jQuery part
+$('.container').mouseover(function(e) {
+    e.preventDefault();
+    $(body).css({"background-color" : `rgb($e.offsetX), 180, offsetY`});
+});
